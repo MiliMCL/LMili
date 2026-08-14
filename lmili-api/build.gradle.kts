2705d4e1ff4e79209bd0c5dfe59661bbe58a552c
xucy <xucy0609@gmail.com>
2026-08-14 15:35:34 +0800

    Upgrade vanniktech plugin to 0.36.0 [no ci]

--- lmili-api/build.gradle.kts
+++ lmili-api/build.gradle.kts
@@ -3,7 +3,7 @@ import paper.libs.com.google.gson.Gson
 
 plugins {
     `java-library`
-    id("com.vanniktech.maven.publish") version "0.30.0"
+    id("com.vanniktech.maven.publish") version "0.36.0"
     signing
     idea
 }
